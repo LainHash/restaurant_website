0f89796deadc898a0dc76b2884ac0024370af774
lain <chiyomiziwari@gmail.com>
2026-09-12 09:58:20 +0700

    feat: add API types for catalog products and ingredients

--- src/types/api/catalog/product.ts
+++ src/types/api/catalog/product.ts
@@ -64,7 +64,7 @@ export interface ProductFilterParams {
   BrandId?: string;
   Keyword?: string;
   SortField?: SortField;
-  Direction?: SortDirection;
+  SortDirection?: SortDirection;
   Page?: number;
   PageSize?: number;
 }
